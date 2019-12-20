75fa70239fb49ca91619cf43b322797f0bd04c6c
Claudio Bebiano <claudiobebiano@gmail.com>
2019-12-20 20:53:00 +0000

    tradução pt

--- locales/pt.lua
+++ locales/pt.lua
@@ -1,8 +1,8 @@
 Locales['pt'] = {
   ['whitelist_check']     = 'a certificar se voce esta na lista de permissoes neste servidor . . .',
-  ['not_whitelisted']     = 'voce nao esta na lista de permissoes neste servidor',
+  ['not_whitelisted']     = 'voce nao esta na lista de permissoes neste servidor!',
   ['steamid_error']       = 'seu teu ID da Steam nao foi encontrado, a Steam esta ligada?',
   ['whitelist_empty']     = 'a lista de permissoes ainda nao foi carregada, ou como alternativa ninguem foi incluido ainda!',
-  ['help_whitelist_add']  = 'adicionar alguem a lista de permissoes',
-  ['help_whitelist_load'] = 'recarregar a lista de permissoes',
+  ['help_whitelist_add']  = 'adicionar alguem a lista de permissoes.',
+  ['help_whitelist_load'] = 'recarregar a lista de permissoes.',
 }
