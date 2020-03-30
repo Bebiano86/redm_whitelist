73f35a9886343896e14014ff463c13b0caaa6afa
Bebiano <claudiobebiano@gmail.com>
2020-03-30 23:09:02 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,9 @@
-game 'rdr3'
+fx_version "adamant"
+games {"rdr3"}
 
-fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
